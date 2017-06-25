58b6c38466e95cea2a6a971a1593055de566027e
rockleeprc <rockleeprc@outlook.com>
2017-06-25 16:10:04 +0800

    修改书籍名称

---
+++ 实战java高并发程序设计/ch5/SemapDemo.java
@@ -0,0 +1,31 @@
+package geym.ch5;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemapDemo implements Runnable{
+    final Semaphore semp = new Semaphore(5);
+    @Override
+    public void run() {
+        try {
+            semp.acquire();
+            //Ä£ÄâºÄÊ±²Ù×÷
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getId()+":done!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally{
+        	semp.release();
+        }
+    }
+    
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(20);
+        final SemapDemo demo=new SemapDemo();
+        for(int i=0;i<20;i++){
+            exec.submit(demo);
+        }
+    }
+}
+
---
+++ 实战java高并发程序设计/ch6/simple/ThreadPool.java
@@ -0,0 +1,80 @@
+package geym.ch6.simple;
+
+import java.util.List;
+import java.util.Vector;
+
+public class ThreadPool 
+{
+    private static ThreadPool instance = null;
+
+    //空闲的线程队列
+    private List<Worker> idleThreads;
+    //已有的线程总数
+    private int threadCounter;
+    private boolean isShutDown = false;
+    
+    private ThreadPool() 
+    {       
+        this.idleThreads = new Vector(5);
+        threadCounter = 0;
+    }
+    
+    public int getCreatedThreadsCount() {
+        return threadCounter;
+    }
+    
+    //取得线程池的实例
+    public synchronized static ThreadPool getInstance() {
+        if (instance == null)
+            instance = new ThreadPool();
+        return instance;
+    }
+   
+    //将线程放入池中
+    protected synchronized void repool(Worker repoolingThread)
+    {
+        if (!isShutDown) 
+        {
+            idleThreads.add(repoolingThread);
+        }
+        else 
+        {
+            repoolingThread.shutDown();//关闭线程
+        }
+    }
+        
+    //停止池中所有线程
+    public synchronized void shutdown()
+    {
+       isShutDown = true;
+       for (int threadIndex = 0; threadIndex < idleThreads.size(); threadIndex++)
+       {
+             Worker idleThread = (Worker) idleThreads.get(threadIndex);
+             idleThread.shutDown();
+       }
+    }
+    
+    //执行任务
+    public synchronized void start(Runnable target)
+    {
+        Worker thread = null; 
+        //如果有空闲线程，则直接使用
+        if (idleThreads.size() > 0) 
+        {
+            int lastIndex = idleThreads.size() - 1;
+            thread = (Worker) idleThreads.get(lastIndex);
+            idleThreads.remove(lastIndex);
+            //立即执行这个任务
+            thread.setTarget(target);
+        }
+        //没有空闲线程，则创建新线程
+        else 
+        { 
+            threadCounter++;
+            // 创建新线程，
+            thread = new Worker(target, "PThread #" + threadCounter, this);
+            //启动这个线程
+            thread.start();
+        }
+    }
+}
